f4a42cd3c2d49c20318aeaa4aa89e66b61f14387
Chandrakant <ckant912@gmail.com>
2022-04-28 23:00:02 +0530

    Login form

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SignIn from './component/signin.component';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        
+        <h1>SIGN IN FORM</h1>
+        <SignIn/>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/component/signin.component.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+
+const SignIn = () => {
+
+     const [name,setName]= useState("");
+     const [email,setEmail]= useState("");
+     const [password,setPassword]= useState("");
+     const [allEntry, setAllEntry]= useState("");
+
+     const submitForm=(event)=>{
+          event.preventDefault();
+          const newEntry= {name:name,email:email,password:password}
+
+          setAllEntry([...allEntry, newEntry]);
+
+          console.log(allEntry);
+     }
+
+     return (
+
+          <>
+               
+               <h2> Welcom to my form </h2>
+               <h2> In this form we will use Regular expresions for Validations </h2>
+
+               <form action="" onSubmit={submitForm}>
+                    <div>
+                         <label htmlFor="name">Name</label>
+                         <input 
+                              type="text" 
+                              name="name"
+                              id="name"
+                              value={name}
+                              onChange={(event)=> setName(event.target.value)}
+                              />
+                    </div>
+
+                    <div>
+                         <label htmlFor="email">Email</label>
+                         <input 
+                              type="text" 
+                              name="email"
+                              id="email"
+                              value={email}
+                              onChange={(event)=> setEmail(event.target.value)}
+
+                              />
+                    </div>
+
+                    <div>
+                         <label htmlFor="password">Password</label>
+                         <input 
+                              type="password" 
+                              name="password"
+                              id="password"
+                              value={password}
+                              onChange={(event)=> setPassword(event.target.value)}
+
+                              />
+                    </div>
+
+                    <button type='submit'>Log In</button>
+               </form>
+
+           
+          </>
+     )
+
+}
+
+export default SignIn;
